df5f5504b53b8f00fdba954977cec05af185b94a
Andrew <andrewfz831@gmail.com>
2023-04-25 21:46:59 -0700

    kermit again

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import MainPage from './components/mainpage';
 import Contact from './components/contact';
+import AboutMe from './components/aboutMe';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainPage/>}/>
         <Route path="/contact" element={<Contact/>}/>
+        <Route path="/aboutMe" element={<AboutMe/>}/>
       </Routes>
       
       </BrowserRouter>
